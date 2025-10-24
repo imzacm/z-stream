89b6e524fdc738116df154b755ee6ee74362f250
Zac McChesney <imzacm@gmail.com>
2025-10-24 19:10:06 +0100

    Re-written RandomFiles - read all files before starting iterator

--- src/finder.rs
+++ src/finder.rs
@@ -1,7 +1,4 @@
-use std::collections::HashMap;
 use std::path::PathBuf;
-
-use camino::Utf8Path;
 
 use crate::media_info::MediaInfo;
 use crate::random_files::RandomFiles;
@@ -12,44 +9,18 @@ pub struct Source {
     pub media_info: MediaInfo,
 }
 
-pub fn start_finder_thread<I>(root_dirs: I, tx: flume::Sender<Source>)
+pub fn start_finder_thread<I>(root_dirs: I, source_tx: flume::Sender<Source>)
 where
     I: IntoIterator<Item: Into<PathBuf>>,
 {
-    let mut random_files = RandomFiles::default().with_roots(root_dirs).cycle_files(true);
+    let random_files = RandomFiles::new(root_dirs);
 
     std::thread::spawn(move || {
         println!("[Finder] Finder task started.");
-        let mut info_map: HashMap<PathBuf, MediaInfo> = HashMap::new();
-        loop {
-            let path = random_files.next_if(|path| {
-                // We've already used this file before.
-                if let Some(media_info) = info_map.get(path) {
-                    return !media_info.is_empty();
-                }
-
-                let Some(path) = Utf8Path::from_path(path) else { return false };
-
-                match MediaInfo::detect(path) {
-                    Ok(media_info) => {
-                        // eprintln!("[Finder] Media info for file {path}: {media_info:?}");
-                        info_map.insert(path.as_std_path().to_path_buf(), media_info);
-                        !media_info.is_empty()
-                    }
-                    Err(_error) => {
-                        // eprintln!("Failed to detect media info for file {path}: {error}");
-                        false
-                    }
-                }
-            });
-
-            let Some(path) = path else { continue };
-
-            let media_info = info_map.get(&path).unwrap();
-            let source = Source { path, media_info: *media_info };
-            if let Err(error) = tx.send(source) {
-                eprintln!("[Finder] Channel closed. Shutting down. {error}");
-                return;
+        for source in random_files {
+            if let Err(error) = source_tx.send(source) {
+                eprintln!("[Finder] Channel closed: {error}");
+                break;
             }
         }
     });
--- src/random_files.rs
+++ src/random_files.rs
@@ -1,178 +1,63 @@
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+use camino::Utf8Path;
 use parking_lot::Mutex;
-use rand::Rng;
-use rand::seq::SliceRandom;
 
-#[derive(Default, Debug)]
+use crate::finder::Source;
+use crate::media_info::MediaInfo;
+
+#[derive(Debug, Clone)]
 pub struct RandomFiles {
     root_dirs: Vec<PathBuf>,
-    files: Arc<Mutex<Vec<PathBuf>>>,
-    file_notify_rx: Option<flume::Receiver<()>>,
-    cycle: bool,
+    files: Vec<Source>,
 }
 
 impl RandomFiles {
-    pub fn with_root<P>(mut self, root: P) -> Self
-    where
-        P: Into<PathBuf>,
-    {
-        self.root_dirs.push(root.into());
-        self
-    }
-
-    pub fn with_roots<I>(mut self, roots: I) -> Self
+    pub fn new<I>(root_dirs: I) -> Self
     where
         I: IntoIterator<Item: Into<PathBuf>>,
     {
-        self.root_dirs.extend(roots.into_iter().map(|r| r.into()));
-        self
-    }
-
-    pub fn cycle_files(mut self, cycle: bool) -> Self {
-        self.cycle = cycle;
-        self
-    }
-
-    fn ensure_files(&mut self) {
-        if !self.cycle || !self.files.lock().is_empty() {
-            return;
-        }
-        if self.file_notify_rx.as_ref().is_none_or(|rx| rx.is_disconnected()) {
-            let rx = walk(self.root_dirs.clone(), self.files.clone());
-            self.file_notify_rx = Some(rx);
-        }
-        if let Some(rx) = &self.file_notify_rx {
-            _ = rx.recv();
-        }
-    }
-
-    pub fn multi_filter_map<T, F>(&mut self, mut map_fn: F) -> Vec<T>
-    where
-        F: FnMut(&Path) -> Option<T>,
-    {
-        self.ensure_files();
-
-        let mut files = self.files.lock();
-
-        let mut filtered = files
-            .iter()
-            .enumerate()
-            .filter_map(|(index, path)| {
-                let item = map_fn(path)?;
-                Some((index, item))
-            })
-            .collect::<Vec<_>>();
-
-        if filtered.is_empty() {
-            return Vec::new();
-        }
-
-        let mut rng = rand::rng();
-        filtered.shuffle(&mut rng);
-
-        let n: usize = if filtered.len() == 1 { 1 } else { rng.random_range(1..filtered.len()) };
-        let mut items = Vec::with_capacity(n);
-
-        filtered.truncate(n);
-        filtered.sort_unstable_by_key(|(index, _)| std::cmp::Reverse(*index));
-        for (index, item) in filtered {
-            files.remove(index);
-            items.push(item);
-        }
-
-        items
-    }
-
-    pub fn multi_if<F>(&mut self, mut if_fn: F) -> Vec<PathBuf>
-    where
-        F: FnMut(&Path) -> bool,
-    {
-        self.ensure_files();
-
-        let mut files = self.files.lock();
-
-        let mut filtered = files
-            .iter()
-            .enumerate()
-            .filter_map(|(index, path)| if if_fn(path) { Some(index) } else { None })
-            .collect::<Vec<_>>();
-
-        if filtered.is_empty() {
-            return Vec::new();
-        }
-
-        let mut rng = rand::rng();
-        filtered.shuffle(&mut rng);
-
-        let n: usize = if filtered.len() == 1 { 1 } else { rng.random_range(1..filtered.len()) };
-        let mut items = Vec::with_capacity(n);
-
-        filtered.truncate(n);
-        filtered.sort_unstable_by_key(|index| std::cmp::Reverse(*index));
-        for index in filtered {
-            let path = files.remove(index);
-            items.push(path);
-        }
-
-        items
-    }
-
-    pub fn next_if<F>(&mut self, mut if_fn: F) -> Option<PathBuf>
-    where
-        F: FnMut(&Path) -> bool,
-    {
-        self.ensure_files();
-
-        let mut files = self.files.lock();
-
-        let filtered = files
-            .iter()
-            .enumerate()
-            .filter_map(|(index, path)| if if_fn(path) { Some(index) } else { None })
-            .collect::<Vec<_>>();
-
-        if filtered.is_empty() {
-            return None;
-        }
-
-        let mut rng = rand::rng();
-        let n: usize = rng.random_range(..filtered.len());
-        let index = filtered[n];
-        Some(files.remove(index))
+        let root_dirs = root_dirs.into_iter().map(Into::into).collect();
+        Self { root_dirs, files: Vec::new() }
     }
 }
 
 impl Iterator for RandomFiles {
-    type Item = PathBuf;
+    type Item = Source;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.ensure_files();
-        let mut files = self.files.lock();
-
-        let mut rng = rand::rng();
-        let index: usize = rng.random_range(..files.len());
-        Some(files.remove(index))
+        if self.files.is_empty() {
+            walk(&self.root_dirs, &mut self.files);
+            println!("[RandomFiles] Shuffling files");
+            let mut rand = urandom::new();
+            rand.shuffle(&mut self.files);
+            println!("[RandomFiles] Finished shuffling files");
+        }
+        self.files.pop()
     }
 }
 
-fn walk(root_dirs: Vec<PathBuf>, files: Arc<Mutex<Vec<PathBuf>>>) -> flume::Receiver<()> {
-    let (tx, rx) = flume::bounded(1);
-    std::thread::spawn(move || {
-        for dir in root_dirs {
-            let files = files.clone();
-            let tx = tx.clone();
-            std::thread::spawn(move || {
-                walk_one(&dir, &files);
-                _ = tx.try_send(());
+fn walk(root_dirs: &[PathBuf], into_files: &mut Vec<Source>) {
+    println!("[RandomFiles] Walking files");
+    let files = Arc::new(Mutex::new(std::mem::take(into_files)));
+    let files_clone = files.clone();
+    std::thread::scope(move |s| {
+        for root_dir in root_dirs {
+            let files = files_clone.clone();
+            s.spawn(move || {
+                walk_one(root_dir, &files);
             });
         }
     });
-    rx
+    let files = Arc::try_unwrap(files).unwrap();
+    let mut files = files.into_inner();
+    files.shrink_to_fit();
+    println!("[RandomFiles] Finished walking files, found {} files", files.len());
+    *into_files = files;
 }
 
-fn walk_one(root_dir: &Path, files: &Mutex<Vec<PathBuf>>) {
+fn walk_one(root_dir: &Path, files: &Mutex<Vec<Source>>) {
     for entry in walkdir::WalkDir::new(root_dir).follow_links(true) {
         let entry = match entry {
             Ok(entry) => entry,
@@ -186,6 +71,15 @@ fn walk_one(root_dir: &Path, files: &Mutex<Vec<PathBuf>>) {
             continue;
         }
 
-        files.lock().push(entry.into_path());
+        let path = entry.into_path();
+
+        let Some(utf8_path) = Utf8Path::from_path(&path) else { continue };
+
+        let Ok(media_info) = MediaInfo::detect(utf8_path) else { continue };
+        if media_info.is_empty() {
+            continue;
+        }
+        let source = Source { path, media_info };
+        files.lock().push(source);
     }
 }
